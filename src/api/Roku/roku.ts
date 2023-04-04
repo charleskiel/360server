705da92631df3407441eb4eab51e1fba671085be
Charles Kiel <CharlesKiel@yahoo.com>
2023-04-04 19:21:53 +0000

    Cleaned some things up.

--- src/api/Roku/roku.ts
+++ src/api/Roku/roku.ts
@@ -1,54 +1,80 @@
 import {
-	MusicVideo, request, Mysql, moment, DownloadFile, User, Channel, Sockets, Content, Runlog, LogEntry, Library
-} from '../modules'
-import { Auth } from '../auth/auth.js';
-import { toUpper } from 'lodash';
+	MusicVideo, request, Sql, moment, DownloadFile, User, Channel, Sockets, Content, Runlog, LogEntry, Library
+} from '../../modules'
+import { Auth } from '../../auth/auth.js';
+import { result, toUpper } from 'lodash';
 import {inherits, log} from 'util';
 import { Socket } from 'socket.io';
 
 export class RokuAPI {
+	private static ROOT_URL = "https://360tv.net/api/v2"
+	private static DEV_URL = "http://192.168.1.101:8080/api/v2"
+	private static URL = RokuAPI.ROOT_URL
 	public static config  =  {
 		output : 0,
-		mainHeader : '360 Music Television',
-		viewpost : 'https://www.360tv.net/api/v1/roku/viewpost',
-		ratepost: 'https://www.360tv.net/api/v1/roku/ratepost',
-		votepost: 'https://www.360tv.net/api/v1/roku/votepost',
-		overlays: 'https://www.360tv.net/api/v1/roku/overlays',
-		viewerStatus : 'https://www.360tv.net/api/v1/roku/viewerStatus',
-		serverStatus: 'https://www.360tv.net/api/v1/roku/serverStatus?',
-		nowPlaying: 'https://www.360tv.net/api/v1/nowPlaying',
-		contentInfo: 'https://www.360tv.net/api/contentInfo',
-		mod: 'https://www.360tv.net/api/v1/roku/mod',
-		broadcastInfo : 'https://www.360tv.net/api/v1/roku/broadcastInfo',
-		userInfo : 'https://www.360tv.net/api/v1/roku/userInfo',
-		UrlBase: 'https://www.360tv.net/api/v1/roku/',
-		UrlGetRegResult : 'https://www.360tv.net/api/v1/roku/getRegCode',
-		UrlGetRegCode : 'https://www.360tv.net/api/v1/roku/getRegResult',
-		UrlWebSite : 'https://www.360tv.net/',
+		mainHeader : `360 Music Television`,
+		viewpostURL : `${URL}/roku/viewpost`,
+		votepostURL : `${URL}/roku/votepost`,
+		ratepostURL : `${URL}/roku/ratepost`,
+		overlaysURL : `${URL}/roku/overlays`,
+		viewerStatusURL : `${URL}/roku/viewerStatus`,
+		serverStatusURL: `${URL}/roku/serverStatus`,
+		nowPlayingURL : `${URL}/nowPlaying`,
+		contentInfoURL : `${URL}/api/contentInfo`,
+		modURL : `${URL}/roku/mod`,
+		broadcastInfoURL : `${URL}/roku/broadcastInfo`,
+		userInfoURL : `${URL}/roku/userInfo`,
+		UrlBaseURL : `${URL}/roku/`,
+		UrlGetRegResultURL : `${URL}/roku/getRegCode`,
+		UrlGetRegCodeURL : `${URL}/roku/getRegResult`,
+		UrlWebSiteURL : `${URL}/`,
 		PositionNotificationPeriod : 10,
+		NowPlayingScreenDisplayTime : 7,
 		WeatherNotificationPeriod : 600,
-		daystoregister : 30,
-		hello: 'https://www.360tv.net/api/v1/roku/theme/hello.wav',
-		notice: 'https://www.360tv.net/api/v1/roku/theme/coin.wav',
-		InterfaceUpdatePeriod: 1000,
-		fonts: [
+		daysToRegister : 30,
+		InterfaceUpdatePeriod : 1000,
+		fonts : [
 			{
 				"name": "Courier New",
-				"url": "https://360tv.net/roku/theme/COURBD.TTF",
+				"url": "https://360tv.net/roku/theme/fonts/COURBD.TTF",
 				"path": "tmp:/fonts/COURBD.TTF",
 				"sizes": [10, 12, 18, 22, 32, 48, 72, 120, 180, 240, 360]
 			},{
 				"name": "Haettenschweiler",
-				"url": "https://360tv.net/roku/theme/HATTEN.TTF",
+				"url": "https://360tv.net/roku/theme/fonts/HATTEN.TTF",
 				"path": "tmp:/fonts/HATTEN.TTF",
 				"sizes": [10, 12, 18, 22, 32, 48, 72, 120, 180, 240, 360]
 			}],
-		"sounds":
+		images: {
+			stars : [
+				"https://www.360tv.net/roku/theme/0star.png",
+				"https://www.360tv.net/roku/theme/1star.png",
+				"https://www.360tv.net/roku/theme/2star.png",
+				"https://www.360tv.net/roku/theme/3star.png" ,
+				"https://www.360tv.net/roku/theme/4star.png",
+				"https://www.360tv.net/roku/theme/5star.png",
+			],
+			averageStars: [
+				
+				"https://www.360tv.net/roku/theme/0star-a.png",
+				"https://www.360tv.net/roku/theme/10star-a.png",
+				"https://www.360tv.net/roku/theme/20star-a.png",
+				"https://www.360tv.net/roku/theme/30star-a.png",
+				"https://www.360tv.net/roku/theme/40star-a.png",
+				"https://www.360tv.net/roku/theme/50star-a.png",
+				"https://www.360tv.net/roku/theme/60star-a.png",
+				"https://www.360tv.net/roku/theme/70star-a.png",
+				"https://www.360tv.net/roku/theme/80star-a.png",
+				"https://www.360tv.net/roku/theme/90star-a.png",
+				"https://www.360tv.net/roku/theme/100star-a.png",
+			]
+		},
+		sounds :
 		{
-			"hello": "https://360tv.net/roku/theme/hello.wav",
-			"notice": "https://360tv.net/roku/theme/coin.wav",
-			"select": "https://360tv.net/demo/theme/select.wav",
-			"dead": "https://360tv.net/roku/theme/dead.wav"
+			"hello": "https://360tv.net/roku/theme/sounds/hello.wav",
+			"notice": "https://360tv.net/roku/theme/sounds/coin.wav",
+			"select": "https://360tv.net/demo/theme/sounds/select.wav",
+			"dead": "https://360tv.net/roku/theme/sounds/dead.wav"
 		}
 	};
 
@@ -57,34 +83,34 @@ export class RokuAPI {
 	public static call(urlParams: any): Promise<any> {
 		
 		return new Promise((ok, fail) => {
-			// console.log(urlParams)
 			const post = urlParams.body;
 
+
 			if (urlParams.method === 'POST') {
-				switch (urlParams.path) {
-					case "/api/v1/roku/config":
+				switch (urlParams._parsedUrl.pathname) {
+					case "/api/v2/roku/config":
 						console.log(`\u001b[31m\x1b[5m\x1b[7mSending launch config to ${post.device.esn}\u001b[0m`);
 						ok(RokuAPI.config);
 						break;
-					case "/api/v1/roku/userInfo":
+					case "/api/v2/roku/userInfo":
 						console.log(`Sending user info to ${post.device.esn}`);
-						ok(User.login(post));
+						User.login(post).then((result: any) => {
+							ok(result);
+						});
 						break;
-					case "/api/v1/roku/status":
+					case "/api/v2/roku/serverStatus":
 						console.log(`Sending status to ${post.device.esn}`);
-						ok(RokuAPI.status());
+						this.serverStatus(urlParams).then((result: any) => {
+							ok(result);
+						});
 						break;
-					case "/api/v1/roku/channelStatus":
-						console.log(`Sending Channel status to ${post.device.esn}`);
-						ok(User.login(post));
-						break;
-					case "/api/v1/roku/overlays":
-						console.log(`Sending overlays to ${post.device.esn}`);
-						// console.log(post);
-						ok(RokuAPI.generateOverlays(post.overlay));
-						break;
-					case "/api/v1/roku/viewpost":
-						console.log(`\u001b[31mSending viewpost to ${post.esn}\u001b[0m for player epoch ${post.playerEpoch}`);
+					case "/api/v2/roku/overlays":
+					case "/api/v2/roku/votepost":
+					case "/api/v2/roku/viewpost":
+						console.log(`\u001b[31m ${urlParams._parsedUrl.pathname}  =>>>> to backend\u001b[0m`);
+						// this.passToBackend(urlParams).then((result: any) => {
+						// 	ok(result);
+						// });
 						RokuAPI.viewpost(post)
 							.then(resp => {
 								ok(resp);
@@ -93,32 +119,6 @@ export class RokuAPI {
 								console.log(`\u001b[31m \x1b[5m \x1b[7m${post.esn} ::: ${err}\u001b[0m`);
 								fail(err);
 							});
-						break;
-					case "/api/v1/roku/votepost":
-						console.log(`\u001b[31mSending Vote post to ${post.esn}\u001b[0m for player epoch ${post.playerEpoch}`);
-						if (post.action == "VOTE") {
-							Sockets.castVote(post.esn, post.voteSelection)
-								.then(resp => {
-									console.log(resp);
-									ok(resp);
-								})
-								.catch(err => {
-									console.log(`\u001b[31m \x1b[5m \x1b[7m${post.esn} ::: ${err}\u001b[0m`);
-									fail(err);
-								});
-						}
-						else if (post.action == "GET") {
-
-							Sockets.getVotes(post.esn)
-								.then(resp => {
-									console.log(resp);
-									ok(resp);
-								})
-								.catch(err => {
-									console.log(`\u001b[31m \x1b[5m \x1b[7m${post.esn} ::: ${err}\u001b[0m`);
-									fail(err);
-								});
-						}
 						break;
 					default:
 						console.log(`\u001b[31m \x1b[5m \x1b[7m Sending defult response to ${post.esn}\u001b[0m`);
@@ -130,20 +130,8 @@ export class RokuAPI {
 			if (urlParams.method === 'GET') {
 				// console.log(urlParams._parsedUrl.query.split("=")[1])
 				switch (urlParams._parsedUrl.pathname) {
-					case "/api/v1/roku/serverStatus":
-						console.log(`Sending server status to ${post.device.esn}`);
-						ok(Channel.status());
-						break
-					case "/api/v1/roku/overlays": {
-						
-						ok(RokuAPI.generateOverlays(urlParams._parsedUrl.query.split("=")[1]));
-						break;
-					}
-					case "/api/v1/roku/status":
-						ok(RokuAPI.status());
-						break;
 					default:
-						console.log("Default response =>>>>" + urlParams._parsedUrl.pathname)
+						console.log("`\u001b[31m \x1b[5m \x1b[7m Default response =>>>>" + urlParams._parsedUrl.pathname + "\u001b[0m")
 						ok(RokuAPI.defaultResponse);
 						break;
 				}
@@ -152,196 +140,77 @@ export class RokuAPI {
 
 
 	}
+	static viewpost(post: any) {
+		console.log(post.method, post.url);
 
-	public static viewpost(post: any) : Promise<any> {
-		// console.log(post);
-		//
-		// return new Promise((ok: any, fail: any) => {
-		// 	const statussql = `
-		// 		SELECT ROUND(UNIX_TIMESTAMP(CURTIME(4)) * 1000) as serverEpoch,
-		// 			runlog.datetime as contentEpoch,
-		// 			(CASE
-		// 				WHEN runlog.contenttype = 0 THEN runlog.datetime + ROUND(videos.trt * 1000)
-		// 				WHEN runlog.contenttype = 1 THEN runlog.datetime + ROUND(bumps.trt * 1000)
-		// 				WHEN runlog.contenttype = 2 THEN runlog.datetime + ROUND(runlog.trt * 1000)
-		// 			END) as contentEndEpoch,
-		//
-		// 			(CASE
-		// 				WHEN runlog.contenttype = 0 THEN videos.trt
-		// 				WHEN runlog.contenttype = 1 THEN bumps.trt
-		// 				WHEN runlog.contenttype = 2 THEN runlog.trt
-		// 			END) as contenttrt,
-		//
-		// 			runlog.overlay, runlog.\`mode\`, runlog.id as runlogid, runlog.contentType, videos.id, videos.alphaArtist, videos.title, videos.trt, videos.titlein, videos.titleout
-		// 			from runlog
-		//
-		// 		left join videos on runlog.contentid = videos.id
-		// 			left join channels on channelNumber = ${post["channelNumber"]}
-		// 			left join bumps on bumps.ID = runlog.contentid
-		//
-		// 		where from_unixtime(${post["playerEpoch"]}/ 1000) <= from_unixtime((datetime / 1000) +
-		// 			(CASE
-		// 				WHEN runlog.contenttype = 0 THEN videos.trt
-		// 				WHEN runlog.contenttype = 1 THEN bumps.trt
-		// 				WHEN runlog.contenttype = 2 THEN runlog.trt
-		// 			END))
-		//
-		// 		order by datetime desc limit 2`;
-		//
-		// 		console.log(statussql)
-		// 	Mysql.query(statussql)
-		// 		.then((results) => { ok(results); })
-		// 		.catch((error) => {
-		// 			console.error(error);
-		// 			fail(error);
-		// 		})
-		// })
-		return new Promise ((ok: any, fail: any)=>{
-			let results: any[] = [];
-			let r: any[] = [];
-			let resultObject: any;
-			let offset = Channel.channels.get(post.channelNumber)?.offset || 35
-			const playerEpoch = post.playerEpoch; // - (offset * 1000);
-			console.log("player Epoch= " + playerEpoch + ` post.playerEpoch - ${offset}` )
-			Channel.channels.get(post.channelNumber)?.runlog.map((logEntry: LogEntry) => {
-				let contentEndEpoch = logEntry.epoch + (logEntry.content.trt * 1000);
-				let timeLeft = (contentEndEpoch - playerEpoch) / 1000;
-				let timeSinceStart =   ((logEntry.epoch - (logEntry.epoch - (logEntry.content.trt * 1000))) / 1000)
-
-				r.push({
-					playerEpoch,
-					now: Date.now(),
-					contentId: logEntry.content.id,
-					...logEntry,
-					playerEpochh: playerEpoch - Date.now(),
-					playerEpochTimestamp: moment.unix(playerEpoch),
-					// contentEpoch: logEntry.epoch,
-					TRT: logEntry.content.trt,
-					contentEndEpoch: contentEndEpoch,
-					timeLeft: timeLeft,
-					ago: moment.unix(timeSinceStart / 1000 ).fromNow(),
-					// ends: moment.unix((logEntry.epoch / 1000) + logEntry.content.trt ).fromNow(),
-					overlay: logEntry.overlay,
-
-				})
-				// console.log(`[${logEntry.content.id}:${logEntry.content.contentType}] `, playerEpoch, logEntry.epoch + (logEntry.content.trt * 1000), (playerEpoch < logEntry.epoch + (logEntry.content.trt * 1000) ) )
-				// console.log("logEntry.epoch       : ", logEntry.epoch)
-				// // console.log("logEntry.content.trt * 1000                   : ", logEntry.content.trt * 1000) 
-				// console.log("logEntry.epoch + trt : ", logEntry.epoch + (logEntry.content.trt * 1000) )
-				// console.log("playerEpoch          : ", playerEpoch)
-				if (playerEpoch < logEntry.epoch + (logEntry.content.trt * 1000 ) ){
-					results.push({
-						// ...logEntry.content,
-						...logEntry,
-						contentEpoch : logEntry.epoch,
-						contentEndEpoch : contentEndEpoch,
-						ago : moment.unix(logEntry.epoch / 1000).fromNow(),
-						left : timeLeft ,
-						ends : moment.unix((logEntry.epoch / 1000) + (logEntry.content.trt * 1000)).fromNow(),
-						overlay : logEntry.overlay,
-						overlays : [...this.generateOverlays(logEntry.overlay)]
-					})
+		return new Promise((result: any, fail: any) => {
+			const options = {
+				headers: "Content-type: text/json;",
+				url: `http://192.168.1.101:8080${post._parsedUrl.pathname}`,
+				method: 'POST',
+				body: post
+			};
+			request(options, function (error: any, response: any, body: any) {
+				console.log(error);
+				if (response && response.statusCode === 200) {
+					if (body != "") {
+						console.log(body);
+						result(JSON.parse(body));
+					} else {
+						fail({
+							StatusMessage: response.statusMessage,
+							StatusCode: response.statusCode,
+						});
+					}
 				}
 			});
-			console.table(r)
-			console.table(results)
-
-			resultObject = {
-				serverEpoch : Date.now(),
-				offset : offset,
-				nowPlaying : results.splice(0,2)
-			}
-			// console.log(JSON.stringify(resultObject, undefined, 4));
-			ok(resultObject);
-			fail(null)
 
 		});
 	}
-	public static votepost(post: any): Promise<any> {
-		console.log(post);
-		return new Promise((ok: any, fail: any) => {
+	
 
+	public static passToBackend(post: any): Promise<any> {
+		console.log(post.method, post.url);
 
-			// Mysql.query(statussql)
-			// 	.then((results) => {
-			// 		console.log(results)
-			// 		ok(results);
-			// 	})
-			// 	.catch((error) => {
-			// 		console.error(error);
-			// 		fail(error);
-			// 	})
-			ok(post);
+		return new Promise ((result: any, fail: any)=>{
+			const options = {
+				headers: "Content-type: text/json;",
+				url: `http://192.168.1.101:8080${post._parsedUrl.pathname}`,
+				method: 'POST',
+				body: post
+			};
+			request(options, function (error: any, response: any, body: any) {
+				console.log(error);
+				if (response && response.statusCode === 200) {
+					if (body != "") {
+						console.log(body)
+						result(JSON.parse(body));
+					} else {
+						fail({
+							StatusMessage: response.statusMessage,
+							StatusCode: response.statusCode,
+						});
+					}
+				}
+			})
+
 		});
 	}
+	
+	public static serverStatus(post: any): Promise<any> {
+		console.log(post.method, post.url);
+		return new Promise((result: any, fail: any) => {
+		
+			Sql.query(`SELECT * FROM [360_development].[360].[channels] order by channelNumber`)
+			.then((data: any) => { result(data.recordsets[0]); })
+				.catch((fail: any) => {
+					fail({
+						error: fail,
+					});
+				});
+		})	
+	};
 
-	static status(): any {
-		let result: any[] = [];
-		Channel.channels.forEach((channel: Channel, key: number) => {
-			result.push({
-				channelNumber: channel.channelNumber,
-				contentID: channel.channelName,
-				url: channel.streamURL,
-				onAir: channel.onAir.valueOf(),
-				offset: channel.offset,
-				upTime: channel.upTime,
-				streamNotificationMessage: channel.streamNotificationMessage,
-				maxViewers: channel.maxViewers,
-				bitrate: 2300,
-				nowPlaying: channel.runlog.splice(0,5)
-			});
-		});
-		return result;
-	}
-
-	public static generateOverlays(overlayType: string) : Array<any> {
-		const overlays = [];
-		// console.log(overlayType.toUpperCase())
-		switch (overlayType.toUpperCase()) {
-			case '360': 
-				overlays.push({
-					type: "poster",
-					x: 0, y: 0,
-					uri: "https://www.360tv.net/roku/theme/overlays/360.png"
-				});
-				break;
-		
-			case 'LATE':
-				overlays.push({
-					type: "poster",
-					x: 0, y: 0,
-					uri: "https://www.360tv.net/roku/theme/overlays/LATE.png"
-				});
-				break;
-			case 'WEEKEND':
-				overlays.push({
-					type: "poster",
-					x: 0, y: 0,
-					uri: "https://www.360tv.net/roku/theme/overlays/WEEKEND.png"
-				});
-				break;
-		
-			case 'VOTE':
-				break;
-		
-			case 'W':
-				overlays.push({
-					type: "poster",
-					x: 0, y: 0,
-					uri: "https://www.360tv.net/roku/theme/overlays/360.png",
-				});
-				break;
-		
-			default:
-				overlays.push({
-					type: "poster", x: 0, y: 0,
-					uri: "https://www.360tv.net/roku/theme/overlays/360.png"
-				});
-				break;
-		}
-		// console.log(overlays)
-		return overlays;
-	}
 	
 }
 
